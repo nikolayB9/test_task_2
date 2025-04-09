690ed44075d18f1314e86201f6c71c92f85b224f
Nick <12nick12nick@gmail.com>
2025-04-09 15:02:51 +0300

    add api categories

---
+++ app/Http/Controllers/API/V1/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Category\IndexCategoryResource;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return IndexCategoryResource::collection(Category::orderBy('order')->get());
+    }
+}
